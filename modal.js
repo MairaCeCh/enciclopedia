601cf0df6a96de0c254408aa37a1f441c617a31f
Mayra Chirino <mairacelestech@hotmail.com>
2023-04-17 19:37:59 -0300

    agregando mas

--- modal.js
+++ modal.js
@@ -2,10 +2,22 @@ const btnAbrirModal = document.querySelector("#btn-abrir-modal");
 const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
 const modal = document.querySelector("#modal");
 
+const btnAbrirModalA = document.querySelector("#btn-abrir-modalA");
+const btnCerrarModalA = document.querySelector("#btn-cerrar-modalA");
+const modalA = document.querySelector("#modalA");
+
 btnAbrirModal.addEventListener("click", () => {
   modal.showModal();
 });
 
 btnCerrarModal.addEventListener("click", () => {
+  modal.close();
+});
+
+btnAbrirModalA.addEventListener("click", () => {
+  modalA.showModal();
+});
+
+btnCerrarModalA.addEventListener("click", () => {
   modal.close();
 });
